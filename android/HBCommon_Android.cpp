3675a0d79b5716b441f46261ddbe94fbbcfb8f13
kikoslee <kikoslee@gmail.com>
2013-01-25 17:36:34 +0800

    安卓支付

--- android/HBCommon_Android.cpp
+++ android/HBCommon_Android.cpp
@@ -46,4 +46,93 @@ int HBUmeng::getParamValue(const char* name)
 	return ::umengGetParamValue(name);
 }
 
+//////////////////////////////////////////////////////////
+// HBPurchase
+//////////////////////////////////////////////////////////
+
+HBPurchase::HBPurchase()
+: mInitialized(false)
+, mTarget(NULL)
+, isPurchasing(false)
+, mSuccessCall(NULL)
+, mFailedCall(NULL)
+{
+}
+    
+void HBPurchase::init(const vector<string>& itemList)
+{
+    mItemList = itemList;
+    mInitialized = true;
+}
+
+string HBPurchase::getItemCost(int index)
+{
+    CCAssert(mInitialized, "HBPurchase must be initialize first.");
+    if (index >= mItemList.size())
+    {
+        CCLog("%s item index[%d] is out of bounds.", __FUNCTION__, index);
+        return "";
+    }
+
+    return getItemCost(mItemList[index].c_str());
+}
+
+string HBPurchase::getItemCost(const char* itemName)
+{
+    CCAssert(mInitialized, "HBPurchase must be initialize first.");
+    return "";
+}
+
+void HBPurchase::purchaseItem(int index, CCObject* target, SEL_CallFuncO successCall, SEL_CallFuncO failedCall)
+{
+    CCAssert(mInitialized, "HBPurchase must be initialize first.");
+    if (index >= mItemList.size())
+    {
+        CCLog("%s item index[%d] is out of bounds.", __FUNCTION__, index);
+        return;
+    }
+    
+    purchaseItem(mItemList[index].c_str(), target, successCall, failedCall);
+}
+    
+void HBPurchase::purchaseItem(const char* itemName, CCObject* target, SEL_CallFuncO successCall, SEL_CallFuncO failedCall)
+{
+    CCAssert(mInitialized, "HBPurchase must be initialize first.");
+
+    vector<string>::iterator itr;
+    bool bFound = false;
+    for (itr = mItemList.begin(); itr != mItemList.end(); ++itr)
+    {
+        if (*itr == itemName)
+        {
+            bFound = true;
+            break;
+        }
+    }
+    
+    if (!bFound)
+    {
+        CCLog("%s item [%s] is out of bounds.", __FUNCTION__, itemName);
+        return;
+    }
+
+    mTarget = target;
+    mSuccessCall = successCall;
+    mFailedCall = failedCall;
+
+	::purchaseItem(itemName);
+}
+
+void HBPurchase::purchaseSuccess(const char* itemName)
+{
+    if (mTarget && mSuccessCall)
+        (mTarget->*mSuccessCall)(CCString::create(itemName));
+}
+
+void HBPurchase::purchaseFailed(int errorCode, const char* failedReason)
+{
+    if (mTarget && mFailedCall)
+        (mTarget->*mFailedCall)(CCString::create(failedReason));
+}
+
 #endif
